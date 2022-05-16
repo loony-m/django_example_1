17da9d392052ab0bb27a1028fbb16d4c5d100a75
loony <wooounding@gmail.com>
2022-05-16 20:52:39 +0300

    initial commit

---
+++ blabla/women/templatetags/women_tags.py
@@ -0,0 +1,28 @@
+from django import template
+from women.models import *
+
+register = template.Library()
+
+@register.simple_tag(name='getcats')
+def get_categories():
+    return Category.objects.all()
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(sort=None, cat_selected=0):
+
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+
+    return {'cats': cats, 'cat_selected': cat_selected}
+
+@register.inclusion_tag('women/menu.html')
+def show_menu():
+    menu = [
+        {'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+    ]
+    return {'menu': menu}
